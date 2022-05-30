0b04cb50cacb0979d0ce8f7896a094dc49185fd1
Jesus Araujo <jesusaraujo.0699@gmail.com>
2022-05-30 12:39:59 -0500

    consultar los intentos

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
  */
 header('Content-Type: application/json; charset=utf-8');
 
-$courseid = $_GET['courseid'];
-$quizid = $_GET['quizid'];
-$userid = $_GET['userid'];
+$courseid = $_GET['courseid'] ?? null;
+$quizid = $_GET['quizid'] ?? null;
+// $userid = $_GET['userid'] ?? null;
 
 function getDataFromWebservice($url)
 {
@@ -27,27 +27,31 @@ $quizid = 37; //ID del quiz de fintra
 $userid = 2343; // ID cindy paola acosta salcedo
 $status = "all";
 
-$stringWebserviceUserAttepms = 'https://caplaft.com/webservice/rest/server.php?wstoken=d54e552bbbb67360046db48dddb6b6a2&wsfunction=mod_quiz_get_user_attempts&quizid=' . $quizid . '&userid=' . $userid . '&status=' . $status . '&moodlewsrestformat=json';
-$request = getDataFromWebservice($stringWebserviceUserAttepms);
+// $cursos = Get_Api('https://caplaft.com/webservice/rest/server.php?wstoken=d54e552bbbb67360046db48dddb6b6a2&wsfunction=core_course_get_courses&moodlewsrestformat=json');
 
-echo json_encode($request);
-
-return;
+// $getUserAttemps = getDataFromWebservice('https://caplaft.com/webservice/rest/server.php?wstoken=d54e552bbbb67360046db48dddb6b6a2&wsfunction=mod_quiz_get_user_attempts&quizid=' . $quizid . '&userid=' . $userid . '&status=' . $status . '&moodlewsrestformat=json');
+// echo json_encode($getUserAttemps);
+// return;
 
 $getCourseGrades = getDataFromWebservice('https://caplaft.com/webservice/rest/server.php?wstoken=d54e552bbbb67360046db48dddb6b6a2&wsfunction=gradereport_user_get_grade_items&courseid=' . $courseid . '&moodlewsrestformat=json');
-// $cursos = Get_Api('https://caplaft.com/webservice/rest/server.php?wstoken=d54e552bbbb67360046db48dddb6b6a2&wsfunction=core_course_get_courses&moodlewsrestformat=json');
 $ratingItems = $getCourseGrades["usergrades"];
-$datajson = [];
+$dataJson = [];
 for ($i = 0; $i < count($ratingItems); $i++) {
     // $user = Get_Api('https://caplaft.com/webservice/rest/server.php?wstoken=c1147432c77ad1942dbde7b82f1def15&wsfunction=core_user_get_users&criteria[0][key]=id&criteria[0][value]='.$califi[$i]["userid"].'&moodlewsrestformat=json');
-    $datajson[$i]["courseid"] = $ratingItems[$i]["courseid"];
-    $datajson[$i]["userid"] = $ratingItems[$i]["userid"];
-    $datajson[$i]["userfullname"] = ucwords(strtolower($ratingItems[$i]["userfullname"]));
-    $datajson[$i]["itemname"] = $ratingItems[$i]["gradeitems"][0]["itemname"];
-    $datajson[$i]["gradeformatted"] =  str_replace("-", "0", str_replace(".", ",", $ratingItems[$i]["gradeitems"][0]["gradeformatted"]));
-    $datajson[$i]["percentageformatted"] = str_replace("-", "0", str_replace(".", ",", $ratingItems[$i]["gradeitems"][0]["percentageformatted"]));
-    $datajson[$i]["dateofissue"] = date('d-m-Y H:i:s', $ratingItems[$i]["gradeitems"][0]["gradedatesubmitted"]);
-    // $datajson[] = $califi[$i][];
+    
+    $dataJson[$i]["courseid"] = $ratingItems[$i]["courseid"];
+    $dataJson[$i]["userid"] = $ratingItems[$i]["userid"];
+    $dataJson[$i]["userfullname"] = ucwords(strtolower($ratingItems[$i]["userfullname"]));
+    $dataJson[$i]["itemname"] = $ratingItems[$i]["gradeitems"][0]["itemname"];
+    $dataJson[$i]["gradeformatted"] =  str_replace("-", "0", str_replace(".", ",", $ratingItems[$i]["gradeitems"][0]["gradeformatted"]));
+    $dataJson[$i]["percentageformatted"] = str_replace("-", "0", str_replace(".", ",", $ratingItems[$i]["gradeitems"][0]["percentageformatted"]));
+    $dataJson[$i]["dateofissue"] = date('d-m-Y H:i:s', $ratingItems[$i]["gradeitems"][0]["gradedatesubmitted"]);
+
+    $getUserAttemps = getDataFromWebservice('https://caplaft.com/webservice/rest/server.php?wstoken=d54e552bbbb67360046db48dddb6b6a2&wsfunction=mod_quiz_get_user_attempts&quizid=' . $quizid . '&userid=' . $ratingItems[$i]["userid"] . '&status=' . $status . '&moodlewsrestformat=json');    
+    $dataJson[$i]["attemps"] = $getUserAttemps;
+
 }
 
-echo json_encode($datajson);
+echo json_encode($dataJson);
+
+// Nuevo
